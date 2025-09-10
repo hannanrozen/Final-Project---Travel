566d5c65da5cf763413557c41ab8b10f5259fd04
Hannan Rozen <hannanrozen45r67@gmail.com>
2025-09-10 17:04:07 +0700

    Update Config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,19 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+          router: ["react-router-dom"],
+        },
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    host: true,
+  },
 });
